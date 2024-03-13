44970f0d0dfbd5c1dbc699a380d69c47eddf1348
Mehrdad S.F. <meddisommer@gmail.com>
2024-03-13 09:59:44 +0330

    some changes - commit13

--- app/src/main/java/com/example/wikipediasample/MainActivity.kt
+++ app/src/main/java/com/example/wikipediasample/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.wikipediasample
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.wikipediasample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
