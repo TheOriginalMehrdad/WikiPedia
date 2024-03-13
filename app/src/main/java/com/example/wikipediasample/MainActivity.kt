8bdd4cd5daf4ccbebdbfc1da424e2c20c1beeddc
Mehrdad S.F. <meddisommer@gmail.com>
2024-03-13 09:14:51 +0330

    drawer ui was created - commit5

--- app/src/main/java/com/example/wikipediasample/MainActivity.kt
+++ app/src/main/java/com/example/wikipediasample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.wikipediasample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.wikipediasample.databinding.ActivityMainBinding
@@ -20,10 +23,57 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-
         setSupportActionBar(binding.mainToolBar)
 
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+
+            this,
+            binding.mainDrawerLayout,
+            binding.mainToolBar,
+            R.string.openDrawer,
+            R.string.closeDrawer
+        )
+        binding.mainDrawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        binding.mainNavigationView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.menu_writer -> {
+
+                    Toast.makeText(this, "Writer", Toast.LENGTH_SHORT).show()
+                    binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+                }
+
+                R.id.menu_photograph -> {
+
+                    Toast.makeText(this, "Photographer", Toast.LENGTH_SHORT).show()
+                    binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+
+                }
+
+                R.id.menu_videoProducer -> {
+
+                    Toast.makeText(this, "Video Producer", Toast.LENGTH_SHORT).show()
+                    binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+
+                }
+
+                R.id.menu_translator -> {
+
+                    Toast.makeText(this, "Translator", Toast.LENGTH_SHORT).show()
+                    binding.mainDrawerLayout.closeDrawer(GravityCompat.START)
+
+                }
+
+                R.id.menu_open_wikipedia -> {}
+                R.id.menu_open_wikimedia -> {}
+            }
+
+            true
+        }
 
     }
 }
