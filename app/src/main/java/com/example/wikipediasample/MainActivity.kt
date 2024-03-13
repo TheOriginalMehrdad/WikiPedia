1e5c7e12313cfe6174e29de247dc42d7b5556049
Mehrdad S.F. <meddisommer@gmail.com>
2024-03-13 12:43:31 +0330

    fragments page was created - commit16

--- app/src/main/java/com/example/wikipediasample/MainActivity.kt
+++ app/src/main/java/com/example/wikipediasample/MainActivity.kt
@@ -7,12 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.wikipediasample.databinding.ActivityMainBinding
+import com.example.wikipediasample.databinding.FragmentProfileBinding
+import com.example.wikipediasample.fragments.ExploreFragment
+import com.example.wikipediasample.fragments.ProfileFragment
+import com.example.wikipediasample.fragments.TrendFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -22,19 +28,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
+///==============================================================================================================================
+
+
+        // To getting my toolbar as an action bar
         setSupportActionBar(binding.mainToolBar)
 
 
+        // Creating navigation drawer
         val actionBarDrawerToggle = ActionBarDrawerToggle(
-
             this,
             binding.mainDrawerLayout,
             binding.mainToolBar,
             R.string.openDrawer,
             R.string.closeDrawer
         )
+
         binding.mainDrawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+
 
         binding.mainNavigationView.setNavigationItemSelectedListener {
 
@@ -68,11 +81,63 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.menu_open_wikipedia -> {}
+
                 R.id.menu_open_wikimedia -> {}
             }
 
             true
         }
 
+        // To be checked fragment explore automatically
+        firstRun()
+
+        binding.mainBottomNavigation.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.menu_btmnvg_explore -> {
+
+                    replaceFragment(ExploreFragment())
+                }
+
+                R.id.menu_btmnvg_trend -> {
+
+                    replaceFragment(TrendFragment())
+                }
+
+                R.id.menu_btmnvg_profile -> {
+
+                    replaceFragment(ProfileFragment())
+                }
+
+            }
+
+            true
+        }
+
+
+        //To prevent creating fragment with duplicate clicks
+        binding.mainBottomNavigation.setOnItemReselectedListener {
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction
+            .replace(R.id.frameMainContainer, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun firstRun() {
+
+        replaceFragment(ExploreFragment())
+        binding.mainBottomNavigation.selectedItemId = R.id.menu_btmnvg_explore
     }
 }
+
+
+
